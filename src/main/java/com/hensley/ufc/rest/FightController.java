a0dd3468985ec51c48eb39ca899692fa54b55121
erichensleyibm <eric.hensley@ibm.com>
2020-07-01 21:33:12 -0400

    add CORS

--- src/main/java/com/hensley/ufc/rest/FightController.java
+++ src/main/java/com/hensley/ufc/rest/FightController.java
@@ -2,6 +2,7 @@ package com.hensley.ufc.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("rest/fight")
 @Api(value="Fight System")
+@CrossOrigin(origins = "http://localhost:8080")
 public class FightController {
 
 	@Autowired
