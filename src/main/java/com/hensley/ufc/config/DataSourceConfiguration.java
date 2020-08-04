a5f6ec5c6ebafb9a0a714a09ed114ac03c8ed487
ehens86 <eghensley@email.wm.edu>
2020-08-04 15:32:00 -0400

    remove cloud auto-config

--- src/main/java/com/hensley/ufc/config/DataSourceConfiguration.java
+++ src/main/java/com/hensley/ufc/config/DataSourceConfiguration.java
@@ -1,28 +1,35 @@
-package com.hensley.ufc.config;
-
-import javax.sql.DataSource;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.Cloud;
-import org.springframework.cloud.CloudFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Configuration
-@Profile("cloud")
-public class DataSourceConfiguration {
-
-  @Bean
-  public Cloud cloud() {
-    return new CloudFactory().getCloud();
-  }
-
-  
-  @Bean
-  @ConfigurationProperties()
-  public DataSource dataSource() {
-    return cloud().getSingletonServiceConnector(DataSource.class, null);
-  }
-
-}
+//package com.hensley.ufc.config;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.cloud.Cloud;
+//import org.springframework.cloud.CloudFactory;
+//import org.springframework.cloud.localconfig.LocalConfigConnector;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//
+//@Configuration
+//@Profile("cloud")
+//public class DataSourceConfiguration {
+//
+//  @Bean
+//  public Cloud cloud() {
+//    return new CloudFactory().getCloud();
+//  }
+//
+//  @Bean
+//  public CloudFactory cloudFactory() {
+//      CloudFactory cf = new CloudFactory();
+//      cf.registerCloudConnector(new LocalConfigConnector());
+//      return cf;
+//  }
+//  
+//  @Bean
+//  @ConfigurationProperties()
+//  public DataSource dataSource() {
+//    return cloud().getSingletonServiceConnector(DataSource.class, null);
+//  }
+//
+//}
